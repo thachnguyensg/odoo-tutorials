muhrizky/tutorials-owl

[Solution] 2.7 Real life update

awesome_dashboard/static/src/dashboard.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, onWillStart } from "@odoo/owl";
+import { Component, onWillStart, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
@@ -13,13 +13,10 @@ class AwesomeDashboard extends Component {
 
     setup() {
         this.action = useService("action");
-        this.dashboardService = useService("awesome_dashboard.dashboard");
+        this.statistics = useState(useService("awesome_dashboard.dashboard"));
         this.display = {
             controlPanel: { "top-right": false, "bottom-right": false },
         };
-        onWillStart(async () => {
-            this.statistics = await this.dashboardService.loadStatistics();
-        });
     }
 
     openCustomerView() {

thachnguyensg/odoo-tutorials

awesome_dashboard/static/src/dashboard.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, onWillStart } from "@odoo/owl";
+import { Component, onWillStart, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
@@ -13,13 +13,10 @@ class AwesomeDashboard extends Component {
 
     setup() {
         this.action = useService("action");
-        this.statistics = useService("awesome_dashboard.statistics");
+        this.statistics = useState(useService("awesome_dashboard.statistics"));
         this.display = {
             controlPanel: {},
         };
-        onWillStart(async () => {
-            this.statistics = await this.statistics.loadStatistics();
-        });
     }
 
     openCustomerView() {