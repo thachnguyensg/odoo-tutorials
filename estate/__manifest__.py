c52ead2e2e961a90c4fd89f6b27c325a1412dd4b
Thach Nguyen <phidaotrang@gmail.com>
2024-06-18 16:25:20 +0700

    feeding data, demo

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,11 +2,15 @@
     "name": "Real Estate",
     "depends": ["base"],
     "data": [
+        "data/estate.property.type.csv",
+        "demo/estate_property.xml",
+        "demo/estate_property_offer.xml",
+        "security/ir.model.access.csv",
         "views/estate_property_views.xml",
         "views/estate_property_type_view.xml",
         "views/estate_property_tag_view.xml",
         "views/estate_menus.xml",
         "views/res_users_view.xml",
-        "security/ir.model.access.csv",
     ],
+    "application": True,
 }
