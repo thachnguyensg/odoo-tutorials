0681f664b8e2ff2fbedc026977f388d3718e0510
Thach Nguyen <phidaotrang@gmail.com>
2024-06-19 17:27:08 +0700

    security yy

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,14 @@
 {
     "name": "Real Estate",
+    "category": "Real Estate/Brokerage",
     "depends": ["base"],
     "data": [
         "data/estate.property.type.csv",
         "demo/estate_property.xml",
+        "demo/estate_property_tag.xml",
         "demo/estate_property_offer.xml",
         "security/ir.model.access.csv",
+        "security/security.xml",
         "views/estate_property_views.xml",
         "views/estate_property_type_view.xml",
         "views/estate_property_tag_view.xml",
--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -7,10 +7,14 @@ class EstateProperty(models.Model):
     def action_sold(self):
         print("I'm overriding the action_sold method")
         print("partner_id", self.buyer_id)
+        journal = self.env["account.journal"].search([("type", "=", "sale")], limit=1)
+        # Another way to get the journal:
+        # journal = self.env["account.move"].with_context(default_move_type="out_invoice")._get_default_journal()
         self.env["account.move"].create(
             {
                 "move_type": "out_invoice",
                 "partner_id": self.buyer_id.id,
+                "journal_id": journal.id,
                 "invoice_line_ids": [
                     Command.create(
                         {
