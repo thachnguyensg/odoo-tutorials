6a0eb3e581841291b3686919e529ab98d922f145
Thach Nguyen <phidaotrang@gmail.com>
2024-06-26 14:35:49 +0700

    separate demo data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,9 +4,6 @@
     "depends": ["base"],
     "data": [
         "data/estate.property.type.csv",
-        "demo/estate_property.xml",
-        "demo/estate_property_tag.xml",
-        "demo/estate_property_offer.xml",
         "security/security.xml",
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
@@ -14,6 +11,11 @@
         "views/estate_property_tag_view.xml",
         "views/estate_menus.xml",
         "views/res_users_view.xml",
+    ],
+    "demo": [
+        "demo/estate_property.xml",
+        "demo/estate_property_tag.xml",
+        "demo/estate_property_offer.xml",
     ],
     "application": True,
 }
