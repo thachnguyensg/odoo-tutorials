5e21f687f16c9f79a23084c1fd4d4aef4b186037
Thach Nguyen <phidaotrang@gmail.com>
2024-07-01 09:18:28 +0700

    real life update sim

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, onWillStart } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
@@ -14,13 +14,10 @@ class AwesomeDashboard extends Component {
     setup() {
         this.action = useService("action");
         const statisticService = useService("awesome_dashboard.statistics");
+        this.statistics = useState(statisticService);
         this.display = {
             controlPanel: {},
         };
-
-        onWillStart(async () => {
-            this.statistics = await statisticService.loadStatistics();
-        });
     }
 
     openCustomerView() {
--- awesome_dashboard/static/src/statistics_service.js
+++ awesome_dashboard/static/src/statistics_service.js
@@ -1,15 +1,18 @@
-/** @odoo-module **/
+/** @odoo-module */
 
 import { registry } from "@web/core/registry";
-import { memoize } from "@web/core/utils/functions";
+import { reactive } from "@odoo/owl";
 
 const statisticsService = {
     dependencies: ["rpc"],
-    async: ["loadStatistics"],
     start(env, { rpc }) {
-        return {
-            loadStatistics: memoize(() => rpc("/awesome_dashboard/statistics")),
-        };
+        const statistic = reactive({ isReady: false });
+        async function loadData() {
+            const upd = await rpc("/awesome_dashboard/statistics");
+            Object.assign(statistic, upd, { isReady: true });
+        }
+        setInterval(loadData, 10000);
+        return statistic;
     },
 };
 
