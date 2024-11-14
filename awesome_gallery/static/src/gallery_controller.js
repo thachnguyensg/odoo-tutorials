1b75b45716e68eb6429075917b22e8f5a07ac5d6
Thach Nguyen <phidaotrang@gmail.com>
2024-11-14 16:49:35 +0700

    add layout

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -1,8 +1,10 @@
 /** @odoo-module **/
 import { Component } from "@odoo/owl";
 import { standardViewProps } from "@web/views/standard_view_props";
+import { Layout } from "@web/search/layout";
 
 export class GalleryController extends Component {
     static template = "awesome_gallery.GalleryController";
+    static components = { Layout };
     static props = { ...standardViewProps };
 }
