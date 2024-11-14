0e86b46833efde067fe7e743f95dcf965236add4
Thach Nguyen <phidaotrang@gmail.com>
2024-11-14 16:07:49 +0700

    hello word view

--- awesome_gallery/static/src/gallery_view.js
+++ awesome_gallery/static/src/gallery_view.js
@@ -1,3 +1,13 @@
 /** @odoo-module */
-
+import { registry } from "@web/core/registry";
+import { GalleryController } from "./gallery_controller";
 // TODO: Begin here!
+export const galleryView = {
+    type: "gallery",
+    display_name: "Awesome Gallery",
+    icon: "fa fa-picture-o",
+    multiRecord: true,
+    Controller: GalleryController,
+};
+
+registry.category("views").add("gallery", galleryView);
