d81aff353e1bb13495830dd55abd485931f1206e
Thach Nguyen <phidaotrang@gmail.com>
2024-11-15 17:27:48 +0700

    load data

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -1,5 +1,6 @@
 /** @odoo-module **/
-import { Component } from "@odoo/owl";
+import { Component, useState, onWillStart, onWillUpdateProps } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 import { standardViewProps } from "@web/views/standard_view_props";
 import { Layout } from "@web/search/layout";
 
@@ -7,4 +8,32 @@ export class GalleryController extends Component {
     static template = "awesome_gallery.GalleryController";
     static components = { Layout };
     static props = { ...standardViewProps };
+
+    setup() {
+        this.orm = useService("orm");
+        this.images = useState({ data: [] });
+        onWillStart(async () => {
+            const { records } = await this.loadImages(this.props.domain);
+            console.log("records", records);
+            this.images.data = records || [];
+        });
+        onWillUpdateProps(async (newProps) => {
+            if (JSON.stringify(newProps.domain) !== JSON.stringify(this.props.domain)) {
+                const { records } = await this.loadImages(newProps.domain);
+                this.images.data = records;
+            }
+        });
+    }
+
+    loadImages(domain) {
+        //async function to load images
+        return this.orm.webSearchRead(this.props.resModel, domain, {
+            specification: {
+                [this.props.archInfo.imageField]: {},
+            },
+            context: {
+                bin_size: true,
+            },
+        });
+    }
 }
