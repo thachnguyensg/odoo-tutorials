1fc3ae95c51f0b8b95fdd1c76243001197ad7497
Thach Nguyen <phidaotrang@gmail.com>
2024-11-18 17:26:47 +0700

    switch view

--- awesome_gallery/static/src/gallery_image.js
+++ awesome_gallery/static/src/gallery_image.js
@@ -2,6 +2,7 @@
 import { Component } from "@odoo/owl";
 import { GalleryModel } from "./gallery_model";
 import { url } from "@web/core/utils/urls";
+import { useService } from "@web/core/utils/hooks";
 
 export class GalleryImage extends Component {
     static template = "awesome_gallery.GalleryImage";
@@ -10,6 +11,10 @@ export class GalleryImage extends Component {
         model: GalleryModel,
     };
 
+    setup() {
+        this.action = useService("action");
+    }
+
     get imageUrl() {
         const imagePath = url("/web/image", {
             id: this.props.record.id,
@@ -17,5 +22,17 @@ export class GalleryImage extends Component {
             field: this.props.model.imageField,
         });
         return imagePath;
+    }
+
+    onImageClick(resId) {
+        debugger;
+        this.action.switchView("form", { resId });
+        // this.action.doAction({
+        //     type: "ir.actions.act_window",
+        //     res_model: this.props.model.resModel,
+        //     res_id: this.props.record.id,
+        //     views: [[false, "form"]],
+        //     target: "current",
+        // });
     }
 }
