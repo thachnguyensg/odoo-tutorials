6cc80a5fb9832fc15b831e0c262b18cf07b850b4
Thach Nguyen <phidaotrang@gmail.com>
2024-11-18 11:08:53 +0700

    abstractions for the future

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -3,41 +3,24 @@ import { Component, useState, onWillStart, onWillUpdateProps } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { standardViewProps } from "@web/views/standard_view_props";
 import { Layout } from "@web/search/layout";
-import { KeepLast } from "@web/core/utils/concurrency";
+import { GalleryRenderer } from "./gallery_renderer";
+import { GalleryModel } from "./gallery_model";
 
 export class GalleryController extends Component {
     static template = "awesome_gallery.GalleryController";
-    static components = { Layout };
+    static components = { Layout, GalleryRenderer };
     static props = { ...standardViewProps };
 
     setup() {
         this.orm = useService("orm");
-        this.images = useState({ data: [] });
-        this.keeplast = KeepLast();
+        this.model = useState(new GalleryModel(this.orm, this.props.resModel, this.props.archInfo));
         onWillStart(async () => {
-            const { records } = await this.loadImages(this.props.domain);
-            console.log("records", records);
-            this.images.data = records || [];
+            await this.model.load(this.props.domain);
         });
         onWillUpdateProps(async (newProps) => {
             if (JSON.stringify(newProps.domain) !== JSON.stringify(this.props.domain)) {
-                const { records } = await this.loadImages(newProps.domain);
-                this.images.data = records;
+                await this.model.load(newProps.domain);
             }
         });
-    }
-
-    loadImages(domain) {
-        //async function to load images
-        return this.keeplast.add(
-            this.orm.webSearchRead(this.props.resModel, domain, {
-                specification: {
-                    [this.props.archInfo.imageField]: {},
-                },
-                context: {
-                    bin_size: true,
-                },
-            })
-        );
     }
 }
