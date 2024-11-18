aabd5f77c8dc61b950d4a2f64bf7ef9bee060549
Thach Nguyen <phidaotrang@gmail.com>
2024-11-18 15:41:34 +0700

    more abstractions

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -3,17 +3,22 @@ import { Component, useState, onWillStart, onWillUpdateProps } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { standardViewProps } from "@web/views/standard_view_props";
 import { Layout } from "@web/search/layout";
-import { GalleryRenderer } from "./gallery_renderer";
-import { GalleryModel } from "./gallery_model";
 
 export class GalleryController extends Component {
     static template = "awesome_gallery.GalleryController";
-    static components = { Layout, GalleryRenderer };
-    static props = { ...standardViewProps };
+    static components = { Layout };
+    static props = {
+        ...standardViewProps,
+        archInfo: Object,
+        Model: Function,
+        Renderer: Function,
+    };
 
     setup() {
         this.orm = useService("orm");
-        this.model = useState(new GalleryModel(this.orm, this.props.resModel, this.props.archInfo));
+        this.model = useState(
+            new this.props.Model(this.orm, this.props.resModel, this.props.archInfo)
+        );
         onWillStart(async () => {
             await this.model.load(this.props.domain);
         });
--- awesome_gallery/static/src/gallery_view.js
+++ awesome_gallery/static/src/gallery_view.js
@@ -2,6 +2,8 @@
 import { registry } from "@web/core/registry";
 import { GalleryController } from "./gallery_controller";
 import { GalleryArchParser } from "./gallery_arch_parser";
+import { GalleryModel } from "./gallery_model";
+import { GalleryRenderer } from "./gallery_renderer";
 // TODO: Begin here!
 export const galleryView = {
     type: "gallery",
@@ -10,6 +12,8 @@ export const galleryView = {
     multiRecord: true,
     Controller: GalleryController,
     ArchParser: GalleryArchParser,
+    Model: GalleryModel,
+    Renderer: GalleryRenderer,
 
     props(genericProps, view) {
         const { ArchParser } = view;
@@ -18,6 +22,8 @@ export const galleryView = {
 
         return {
             ...genericProps,
+            Model: view.Model,
+            Renderer: view.Renderer,
             archInfo,
         };
     },
